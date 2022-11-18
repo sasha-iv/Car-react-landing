b039d52c65ef352e2e680c6db0ce1c59602e3c68
sasha-iv <ovandolyak@gmail.com>
2022-11-18 23:31:55 +0200

    add default message after submit contact form

--- src/Components/Main/Contacts/ContactForm/ContactForm.jsx
+++ src/Components/Main/Contacts/ContactForm/ContactForm.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import './ContactForm.scss';
 
 
 const ContactForm = () => {
+
+    const [alreadySent, setAlreadySent] = useState(false);
+    const toggleDisplay = () => setAlreadySent(value => !value);
+
     return(
         <div className='contactFormContainer'>
-           <form className='contactForm'>
-               <div>
-                   <input type='text' placeholder='Your name'/>
-                   <input type='text' placeholder='Your email'/>
-               </div>
-               <input type='text' placeholder='Subject'/>
-               <textarea placeholder='Message'></textarea>
-               <button className='btnStyle'>Send message</button>
-           </form>
+            {alreadySent
+                ?<div className='contactForm'>
+                    <p>Thank you for your message. <br/> We will contact you soon.</p>
+                </div>
+
+                : <form className='contactForm'>
+                    <div>
+                        <input type='text' placeholder='Your name'/>
+                        <input type='text' placeholder='Your email'/>
+                    </div>
+                    <input type='text' placeholder='Subject'/>
+                    <textarea placeholder='Message'></textarea>
+                    <button className='btnStyle' onClick={toggleDisplay}>Send message</button>
+                </form>
+            }
         </div>
     )
 }
